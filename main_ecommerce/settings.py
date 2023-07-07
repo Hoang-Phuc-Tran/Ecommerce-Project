bdfce0e1258fe6a443e4b2968744edd0b69cc9b2
ptran9102 <hoangphuctran9102@gmail.com>
2023-07-07 16:10:55 -0400

    Registration, Login with Token Based Verification & Message Alerts

--- main_ecommerce/settings.py
+++ main_ecommerce/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -137,3 +136,9 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "Danger",
+}
