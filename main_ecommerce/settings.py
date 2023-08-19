c711f227c8c5bcc5365b43af9190cdc87497aa0c
ptran9102 <hoangphuctran9102@gmail.com>
2023-08-18 20:42:32 -0400

    Deploying to Railway

--- main_ecommerce/settings.py
+++ main_ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-ecommerce-env.eba-ebcirpy3.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -87,24 +87,13 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if 'RDS_DB_NAME' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
@@ -164,8 +153,8 @@ MESSAGE_TAGS = {
 
 CSRF_TRUSTED_ORIGINS=['http://localhost', 'http://127.0.0.1:8000']
 
-EMAIL_HOST = config('EMAIL_HOST')  
-EMAIL_PORT = config('EMAIL_PORT', cast=int)  
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')  
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)  
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
