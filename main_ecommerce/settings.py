629684071f8bbfceb04e099e9d69d193ec7c6885
ptran9102 <hoangphuctran9102@gmail.com>
2023-08-18 23:13:08 -0400

    Deploying to Railway

--- main_ecommerce/settings.py
+++ main_ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ecommerce-project-production-ed55.up.railway.app']
+ALLOWED_HOSTS = ["*"]
 
 #CSRF_TRUSTED_ORIGINS = ['https://shoppingonline.up.railway.app', 'shoppingonline.up.railway.app', 'www.shoppingonline.up.railway.app', 'http://shoppingonline.up.railway.app']
 
