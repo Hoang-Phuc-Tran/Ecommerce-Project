1a63bf410ee10000a14b7c438ce26baca6d8ea39
ptran9102 <hoangphuctran9102@gmail.com>
2023-08-18 22:54:32 -0400

    Deploying to Railway

--- main_ecommerce/settings.py
+++ main_ecommerce/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'shoppingonline.up.railway.app']
 
 #CSRF_TRUSTED_ORIGINS = ['https://shoppingonline.up.railway.app', 'shoppingonline.up.railway.app', 'www.shoppingonline.up.railway.app', 'http://shoppingonline.up.railway.app']
 
-CSRF_TRUSTED_ORIGINS = ['https://shoppingonline.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://shoppingonline.up.railway.app','http://shoppingonline.up.railway.app']
 # Application definition
 
 INSTALLED_APPS = [
@@ -48,11 +48,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
